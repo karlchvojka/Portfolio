94060a9c8b7ae1074100250ad469550c67414bc1
Karl Chvojka <karl.chvojka@protonmail.com>
2020-12-28 11:20:13 -0500

    Updating template

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,44 @@
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
 const htmlPlugin = new HtmlWebpackPlugin({
   template: "./src/index.html",
   filename: "./index.html"
-})
+});
 
 module.exports = {
   entry: "./src",
   output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, "build/"),
+    publicPath: "/",
+    filename: "bundle.js"
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)/,
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: ['babel-loader'],
       },
-      {
-       test: /\.module\.s(a|c)ss$/,
-        loader: [
-         isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              sourceMap: isDevelopment
-            }
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: isDevelopment
-            }
-          }
-       ]
-      },
-      {
-        test: /\.s(a|c)ss$/,
-        exclude: /\.module.(s(a|c)ss)$/,
-        loader: [
-          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-         'css-loader',
-         {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: isDevelopment
-            }
-          }
-        ]
-      }
     ],
   },
 
   resolve: {
-    extensions: ["*", ".js", ".jsx", '.scss'],
+    extensions: ["*", ".js", ".jsx", '.scss', '.css'],
   },
   plugins: [
     htmlPlugin,
@@ -66,5 +46,11 @@ module.exports = {
       filename: isDevelopment ? '[name].css' : '[name].[hash].css',
       chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
     })
-  ]
+  ],
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  }
 };
