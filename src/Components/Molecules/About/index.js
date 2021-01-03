a0fbaf829f74132724bcd387d540fed018268d79
karlchvojka <karl.chvojka@protonmail.com>
2021-01-03 17:41:42 -0500

    Skills are now in place

--- src/Components/Molecules/About/index.js
+++ src/Components/Molecules/About/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import Header from '../../Atoms/Header';
 //Styles
 
 function About() {
   return (
     <section>
-      <h2>About</h2>
-      <p className="firstP">I am a Full-stack developer with 10 years of industry experience building websites, advertising campaigns, interacting with traditional media, and working in teams of people from a variety of disciplines.</p>
+      <Header type='h2' text='About' />
+      <p>I am a Full-stack developer with 10 years of industry experience building websites, advertising campaigns, interacting with traditional media, and working in teams of people from a variety of disciplines.</p>
       <p> I have an unending curiosity for new ideas and processes that helps expand my work. The advancement of Technology Continues to bewilder and inspires me.</p>
     </section>
   )
---
+++ src/Components/Molecules/List/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+
+// Styles.
+const StyledList = styled.ul`
+  list-style: none;
+  padding-left: 0px;
+
+  li {
+    p {
+      margin: 0px 0px 5px 0px;
+    }
+  }
+`;
+
+function List({ items }) {
+  const listMap = items.map((item) => {
+    return (
+      <li>
+        <p>{item}</p>
+      </li>
+    )
+  })
+  return (
+    <StyledList>
+      {listMap}
+    </StyledList>
+  )
+}
+
+export default List;
