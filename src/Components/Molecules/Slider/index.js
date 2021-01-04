5f8fca034d561eedffefc216964b8891597aefd1
karlchvojka <karl.chvojka@protonmail.com>
2021-01-04 16:46:14 -0500

    Added responsive code for slider

--- src/Components/Molecules/Slider/index.js
+++ src/Components/Molecules/Slider/index.js
@@ -7,24 +7,91 @@ const StyledSlider = styled.section`
   background-image:url(${karlSlider});
   background-position:center;
   background-repeat:no-repeat;
-  height:600px;
-  margin:0 auto;
+  background-size:cover;
   display:flex;
+  height:300px;
+  margin:0 auto;
 
   div {
     align-self: center;
-    width: 40%;
     margin: 0 30px;
-
+    width: 50%;
 
     h1 {
-      text-shadow: 3px 3px 2px #000;
       color: #00abdf;
+      font-size: 25px;
       display: block;
+      text-shadow: 3px 3px 2px #000;
     }
     p {
-      font-size: 18px;
       color:#fff;
+      font-size: 14px;
+    }
+  }
+
+  @media (min-width: 576px) {
+    height:300px;
+
+    div {
+      width: 50%;
+      h1 {
+        font-size: 20px;
+      }
+      p {
+        font-size: 14px;
+      }
+    }
+  }
+  @media (min-width: 768px) {
+    height:350px;
+
+    div {
+      width: 50%;
+      h1 {
+        font-size: 25px;
+      }
+      p {
+        font-size: 16px;
+      }
+    }
+  }
+  @media (min-width: 992px) {
+    height:400px;
+
+    div {
+      width: 50%;
+      h1 {
+        font-size: 30px;
+      }
+      p {
+        font-size: 18px;
+      }
+    }
+  }
+  @media (min-width: 1200px) {
+    height:600px;
+
+    div {
+      width: 50%;
+      h1 {
+        font-size: 30px;
+      }
+      p {
+        font-size: 18px;
+      }
+    }
+  }
+  @media (min-width: 1400px) {
+    height:600px;
+
+    div {
+      width: 50%;
+      h1 {
+        font-size: 30px;
+      }
+      p {
+        font-size: 18px;
+      }
     }
   }
 `;
