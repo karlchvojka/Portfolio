273722c05afad74ee2392049eb10c141dd7a8218
karlchvojka <karl.chvojka@protonmail.com>
2021-01-03 19:03:08 -0500

    init Project Organism

---
+++ src/Components/Organisms/Projects/index.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+// Component import
+import Header from '../../Atoms/Header';
+
+// Styles
+const StyledProjects = styled.section`
+`;
+
+function Projects() {
+  return (
+
+  )
+}
+
+export default Projects;
