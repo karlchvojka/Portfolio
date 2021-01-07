1f6c289d97082f48027f8026b1bdd1da98288a0e
karlchvojka <karl.chvojka@protonmail.com>
2021-01-07 16:08:05 -0500

    implemented project responsive

--- src/Components/Organisms/Projects/index.js
+++ src/Components/Organisms/Projects/index.js
@@ -14,8 +14,76 @@ const StyledProjects = styled.section`
     display: flex;
     flex-wrap: wrap;
     flex-direction: row;
-    margin-right: -15px;
-    margin-left: -15px;
+    margin-right: 5%;
+    margin-left: 5%;
+
+    article {
+      max-width: 90%;
+      flex: 0 0 90%;
+    }
+  }
+
+  // Small devices (landscape phones, 576px and up)
+  @media (min-width: 576px) {
+    .projWrap {
+      margin-right: 5%;
+      margin-left: 5%;
+      article {
+        max-width: 90%;
+        flex: 0 0 90%;
+      }
+    }
+  }
+
+  // Medium devices (tablets, 768px and up)
+  @media (min-width: 768px) {
+    .projWrap {
+      margin-right: 0%;
+      margin-left: 0%;
+      article {
+        max-width: 46%;
+        flex: 0 0 46%;
+      }
+    }
+  }
+
+  // Large devices (desktops, 992px and up)
+  @media (min-width: 992px) {
+    .projWrap {
+      margin-right: 0;
+      margin-left: 0;
+
+      article {
+        max-width: 30%;
+        flex: 0 0 30%;
+      }
+    }
+  }
+
+  // X-Large devices (large desktops, 1200px and up)
+  @media (min-width: 1200px) {
+    .projWrap {
+      margin-right: 0;
+      margin-left: 0;
+
+      article {
+        max-width: 30%;
+        flex: 0 0 30%;
+      }
+    }
+  }
+
+  // XX-Large devices (larger desktops, 1400px and up)
+  @media (min-width: 1400px) {
+    .projWrap {
+      margin-right: 0;
+      margin-left: 0;
+
+      article {
+        max-width: 30%;
+        flex: 0 0 30%;
+      }
+    }
   }
 `;
 
