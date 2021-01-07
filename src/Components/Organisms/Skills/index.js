051d76f49f204a4925fc2c5326c96d430b46bd84
karlchvojka <karl.chvojka@protonmail.com>
2021-01-07 15:49:53 -0500

    Implemented responsive css for skills

--- src/Components/Organisms/Skills/index.js
+++ src/Components/Organisms/Skills/index.js
@@ -9,23 +9,138 @@ import List from '../../Molecules/List';
 const StyledSkills = styled.section`
   border-top: 1px solid #00abdf;
 
+  h2 {
+  }
   .listWrap {
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
 
     div {
-      max-width: 25%;
-      flex: 0 0 25%;
+      max-width: 50%;
+      flex: 0 0 50%;
 
       h3 {
         color:#00abdf;
-        font-size:22px;
+        font-size:19px;
+        margin-top: 0px;
       }
 
       li {
         p {
           font-weight: 700;
+          font-size: 16px;
+          line-height:1.5;
+          margin-bottom: 5px;
+        }
+      }
+    }
+  }
+
+  @media (min-width: 576px) {
+    padding-bottom:20px;
+
+    .listWrap {
+
+    div {
+      max-width: 50%;
+      flex: 0 0 50%;
+
+      h3 {
+        font-size:19px;
+      }
+
+      li {
+        p {
+          font-size: 16px;
+          line-height:1.5;
+          margin-bottom: 5px;
+        }
+      }
+    }
+  }
+  }
+  @media (min-width: 768px) {
+    padding-bottom:20px;
+
+    .listWrap {
+
+    div {
+      max-width: 25%;
+      flex: 0 0 25%;
+
+      h3 {
+        font-size:19px;
+      }
+
+      li {
+        p {
+          font-size: 16px;
+          line-height:1.5;
+          margin-bottom: 5px;
+        }
+      }
+    }
+  }
+  @media (min-width: 992px) {
+    padding-bottom:20px;
+
+    .listWrap {
+
+    div {
+      max-width: 25%;
+      flex: 0 0 25%;
+
+      h3 {
+        font-size:19px;
+      }
+
+      li {
+        p {
+          font-size: 16px;
+          line-height:1.5;
+          margin-bottom: 5px;
+        }
+      }
+    }
+  }
+  @media (min-width: 1200px) {
+    padding-bottom:20px;
+
+    .listWrap {
+
+    div {
+      max-width: 25%;
+      flex: 0 0 25%;
+
+      h3 {
+        font-size:22px;
+      }
+
+      li {
+        p {
+          font-size: 18px;
+          line-height:1.5;
+          margin-bottom: 5px;
+        }
+      }
+    }
+  }
+  @media (min-width: 1400px) {
+    padding-bottom:20px;
+
+    .listWrap {
+
+    div {
+      max-width: 25%;
+      flex: 0 0 25%;
+
+      h3 {
+        font-size:22px;
+      }
+
+      li {
+        p {
           font-size: 18px;
           line-height:1.5;
           margin-bottom: 5px;
