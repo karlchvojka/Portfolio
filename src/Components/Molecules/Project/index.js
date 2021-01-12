be6dfa5e638ede305fc0b721960d064ad1d78a4a
karlchvojka <karl.chvojka@protonmail.com>
2021-01-12 16:19:31 -0500

    Finalized styles

--- src/Components/Molecules/Project/index.js
+++ src/Components/Molecules/Project/index.js
@@ -5,15 +5,29 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const StyledProject = styled.article`
-  margin: 20px auto 0 auto;
-  padding: 10px;
   background-color: #fff;
   border: 1px solid #00abdf;
   border-radius: 5px;
-  min-height:510px;
-  height: auto;
   flex: 0 0 29%;
+  height: auto;
+  margin: 20px auto 0 auto;
+  min-height:510px;
   max-width: 29%;
+  padding: 10px;
+  position: relative;
+
+  h3, h4 {
+    margin-top: 0px;
+    margin-bottom: 20px;
+  }
+
+  h4 {
+    font-size:16px;
+  }
+
+  p {
+    margin: 0px 0px 20px 0px;
+  }
 
   .imgWrap {
     overflow: hidden;
@@ -24,6 +38,22 @@ const StyledProject = styled.article`
       width: 100%;
       min-height: 200px;
       height: auto;
+    }
+  }
+
+  .gitLink{
+    bottom: 0;
+    margin-bottom: 10px;
+    position: absolute;
+
+    a:link, a:visited {
+      color: #007bff;
+      text-decoration: none;
+    }
+
+    a:hover {
+      color: #0056b3;
+      text-decoration: underline;
     }
   }
 `;
@@ -38,7 +68,7 @@ function Project({ name, img, desc, tech, git }) {
       {desc}
       <h4>Technology Used:</h4>
       <p>{tech}</p>
-      <p><a href={git}><FontAwesomeIcon icon={faGithub}/> View project</a></p>
+      <p className="gitLink"><a href={git}><FontAwesomeIcon icon={faGithub}/> View project</a></p>
     </StyledProject>
   )
 };
