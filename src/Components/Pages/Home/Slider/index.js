9e06b31eee6bfa4723aba534072aaa09e5b74877
karlchvojka <karl.chvojka@protonmail.com>
2021-01-14 17:18:51 -0500

    Spelling

--- src/Components/Pages/Home/Slider/index.js
+++ src/Components/Pages/Home/Slider/index.js
@@ -8,7 +8,7 @@ function Slider() {
       <div>
         <Header type='h1' text="Heya! I'm Karl Chvojka." />
         <p>I am a Full-stack developer with 10 years of industry experience building websites, advertising campaigns, interacting with traditional media, and working in teams of people from a variety of disciplines.</p>
-        <p> I have an unending curiosity for new ideas and processes that helps expand my work. The advancement of Technology Continues to bewilder and inspire me.</p>
+        <p> I have an unending curiosity for new ideas and processes that helps expand my work. The advancement of technology continues to bewilder and inspire me.</p>
       </div>
     </StyledSlider>
   )
