b07a24e15ed0657a3e0afafb822badc10dd17158
Karl Chvojka <karl.chvojka@protonmail.com>
2021-01-13 21:17:56 -0500

    Workplace restructure.

--- src/Components/Pages/Resume/index.js
+++ src/Components/Pages/Resume/index.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+
+// Component Includes
+import Workplace from './Workplace';
+
+// Asset Includes
 import hexRepeat from '../../../Assets/Images/hexrepeat2.jpg';
+import expData from './expData';
 
 // Styles
 const StyledResume = styled.div`
@@ -217,7 +223,7 @@ const StyledResume = styled.div`
     .skillsWrap{
 
       .listWrap {
-        
+
         div {
           max-width: 25%;
           flex: 0 0 25%;
@@ -228,73 +234,8 @@ const StyledResume = styled.div`
 `;
 
 function Resume() {
-  const [workExp, setWorkExp] = useState([
-    {
-      placementName: "Lighthouse Labs",
-      position: "Front-End Developer",
-      dates: "Aug 2019 - Mar 2020",
-      keywords: "Customer serivce, Student Mentorship, Project Management, Marketing, Front End Development (HTML5, CSS3, CSS Grid, Flexbox), JavaScript, jQuery, Bootstrap, React, Ruby on Rails, JSX, Active Record",
-      desc: "Working with the Marketing team to plan and execute the building of Static ReactJs sites for marketing and promotional purposes. Working with the internal staff to maintain and upgrade internal systems in React, Ruby on Rails, etc."
-    },
-    {
-      placementName: "Freelance Work",
-      position: "Full Stack Developer",
-      dates: "Aug 2010 - Present",
-      keywords: "Business Management, Client Interaction, Client relations, Customer serivce, Project Management, Self Employment, Front End Development (HTML5, CSS3), Wordpress, PHP, MySQL, JavaScript, jQuery, Bootstrap, Photography, User Guide Creation.",
-      desc: "Managing and completing websites for a variety of clients. Projects are handled from initial contact with the client to the final launch including Concepts, Wireframing, Design, Development, Migration, Content Management, Testing, and Launching. Supplying clients with Hosting and Host maintenance, including Server updates, and Hosting provider interaction when needed."
-    },
-    {
-      placementName: "FreeCodeCamp",
-      position: "Global Community Manager",
-      dates: "Aug 2017 - Apr 2020",
-      keywords: "Social Media Management, Facebook Group moderation, Community Organizing, Public speaking",
-      desc: "FreeCodeCamp is an online learning resource for Fullstack Development learning. FreeCodeCamp has broken learning Web Development into digestible chunks to allow people to learn how to code in their spare time. As a Global Community Manager, my biggest input into the FCC ecosystem is managing the moderators for the Facebook Groups and other social media."
-    },
-    {
-      placementName: "MediaFace, Toronto",
-      position: "Fullstack Web Developer",
-      dates: "June 2017 - Oct 2017",
-      keywords: "Client Research, Client relations, Front End Development (HTML5, CSS3), Wordpress, PHP, MySQL, Javascript, jQuery, Bootstrap, Technical SEO Implementation.",
-      desc: "Building Responsive Worpdress Themes with Bootstrap. Tasks included building full websites, and Wordpress Plugins. As well as setting up SEO Tracking codes for Google Analytics, Facebook, and Bing. "
-    },
-    {
-      placementName: "Digital Shift Marketing, Toronto",
-      position: "Front End Developer",
-      dates: "Feb 2016 - Oct 2016",
-      keywords: "Development Team Management Training, Process writing, Client Research, Client relations, Customer service Project Management, Front End Development (HTML5, CSS3), Wordpress, PHP, MySQL, Javascript, jQuery, Bootstrap, Technical SEO Implementation.",
-      desc: "Building Responsive Worpdress Themes with Bootstrap. Tasks included building full websites to building landing pages for SEO A/B Testing. As well as setting up SEO Tracking codes for Google Analytics, Facebook, and Bing. Management duties included heading projects, as well as writing processes and procedures to be used by the development department and managing the IT needs of the company."
-    },
-    {
-      placementName: "VentureWeb, Toronto",
-      position: "Web Consultant, Front End Developer",
-      dates: "Sept 2013  –  Dec 2014",
-      keywords: "Development Team Management Training, Process writing, Client Research, Client relations, Customer service Project Management, Front End Development (HTML5, CSS3), Wordpress, PHP, MySQL, Javascript, jQuery, Bootstrap, Technical SEO Implementation.",
-      desc: "Working as a web consultant through VentureWeb for Accenture, as a site manager for Accenture's Careers Websites. My tasks included building full websites to building landing pages for SEO A/B Testing. As well as setting up SEO Tracking codes for Google Analytics, Facebook, and Bing."
-    },
-    {
-      placementName: "Northlands, Edmonton",
-      position: "Sr. Front End Developer",
-      dates: "Sept 2012  –  Apr 2013",
-      keywords: "Front End Development (HTML5, CSS3), JavaScript, jQuery, PHP, Wordpress, Drupal, Joomla!, User guide creation, Content Management, Web Platform Maintenance, Server Maintenance, Enterprise Level Company",
-      desc: "Primary responsibility for position was the updating and maintenance of the 13 web properties for Northlands. Acted as the primary contact for the hosting companies, and IT service providers that Northlands works with. This included managing, updating and maintaining the web servers, as well as working with the hosting companies to coordinate hardware maintenance and downtime. Additionally, responsible for internal system creation for managing new content to be added to the web properties."
-    }
-  ]);
+  const workMap = expdata.map(work => <Workplace {...work} />);
 
-  const workMap = workExp.map((work) => {
-    return (
-      <article className="workPlace">
-        <h3>{work.placementName}</h3>
-        <section className="workexpHeader">
-          <h4>{work.position}</h4>
-          <p>{work.dates}</p>
-        </section>
-        <p className="keywords"><span>Noteable Keywords: </span>
-          {work.keywords}
-        </p>
-        <p>{work.desc}</p>
-      </article>
-    )
-  });
   return (
     <StyledResume>
       <section className="controlWrap">
