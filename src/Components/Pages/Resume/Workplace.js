1ec3984cfd76b0bf74b85552708840b2c4eb4415
Karl Chvojka <karl.chvojka@protonmail.com>
2021-01-13 21:17:43 -0500

    Exported Workplace Data

---
+++ src/Components/Pages/Resume/Workplace.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function Workplace({
+  dates,
+  desc,
+  keywords,
+  placementName,
+  position,
+}) {
+  return (
+    <article className="workPlace">
+      <h3>{placementName}</h3>
+      <section className="workexpHeader">
+        <h4>{position}</h4>
+        <p>{dates}</p>
+      </section>
+      <p className="keywords"><span>Noteable Keywords: </span>
+        {keywords}
+      </p>
+      <p>{desc}</p>
+    </article>
+  )
+}
