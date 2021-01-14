94b2c63bd96621ca0a2f2370b3b0cbc0e6fd78a5
Karl Chvojka <karl.chvojka@protonmail.com>
2021-01-13 21:17:17 -0500

    Fixed imports

--- src/Components/Pages/Home/index.js
+++ src/Components/Pages/Home/index.js
@@ -1,32 +1,21 @@
-import React from 'react';
 import { styled, createGlobalStyle } from 'styled-components';
-import { Helmet } from 'react-helmet';
-
-// Style Includes
-import { GlobalStyle } from '../../../GlobalStyles.js';
 
 // Component Includes
-import Header from '../../Organisms/Header';
-import Slider from '../../Molecules/Slider';
-import Skills from '../../Organisms/Skills';
-import Projects from '../../Organisms/Projects';
-import Footer from '../../Organisms/Footer';
+import Header from 'organisms/Header';
+import Slider from 'molecules/Slider';
+import Skills from 'organisms/Skills';
+import Projects from 'organisms/Projects';
+import Footer from 'organisms/Footer';
 
 function Home() {
   return (
-    <div className="App">
-      <Helmet>
-        <title>Karl Chvojka's Portfolio</title>
-        <meta name="description" content="The Web Development Portfolio for Karl Chvojka." />
-        <meta name="keywords" content="Karl Chvojka, Web Development, React, Ruby on Rails" />
-      </Helmet>
-      <GlobalStyle />
+    <>
       <Header />
       <Slider />
       <Skills />
       <Projects />
       <Footer />
-    </div>
+    </>
   )
 }
 
